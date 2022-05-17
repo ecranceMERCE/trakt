ebbd811627c241f12f41b3a821ff8c9c556a4e39
Cyril Cohen <cohen@crans.org>
2022-05-17 13:02:43 +0200

    adding toolbox

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/148df9e42e2d0222a025654e4bd5535f986178d1.tar.gz;
+    sha256 = "0shlh97kzx3w1lj92v6mnaz2klg7ha1ns5df4r3f5cjjkgv3g2i6";
+  }
